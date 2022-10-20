0d96cb426d003319f6965c474c34c33d485dfad9
Evans Ansong <65941289+evans646@users.noreply.github.com>
2022-10-20 13:56:34 -0400

    feat: optimizing

--- REF/client/src/actions/upvoteActions.js
+++
@@ -1,9 +0,0 @@
-
-
-
-export const SET_USER_UPVOTE_SUCCESS = 'SET_USER_UPVOTE_SUCCESS'
-
-export const setUpvoteOnArticle = upvotes=> ({
-    type:SET_USER_UPVOTE_SUCCESS,
-    payload: { upvotes},
-});
